12f4301854dd31151254b0ac87a7bebb2aed500f
Santos Carvalhais <santos@dukar.com.br>
2022-09-23 09:46:06 -0300

    alterados

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-from app import routes
+from app import app
+import os
+
+if __name__== 'main':
+    port = int(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port = port)
